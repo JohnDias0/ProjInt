148236e93d3748de52c6b3c4949b8d40b7339abe
JohnDias0 <dias44520@gmail.com>
2024-08-12 22:12:59 -0300

    feat: component Intentions

--- src/app/shared/features/Intentions/components/intentions/intentions.component.ts
+++ src/app/shared/features/Intentions/components/intentions/intentions.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-intentions',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './intentions.component.html',
   styleUrl: './intentions.component.css'
 })
 export class IntentionsComponent {
+  campos: { [key: string]: string } = {};
+  maxCampos: number = 8; 
+  @ViewChildren('campo') campoRefs!: QueryList<ElementRef>;
+
+  constructor() {
+    this.campos['Falecidos até 30 dias'] = '';
+    this.campos['Falecidos com mais de 30 dias'] = '';
+    this.campos['Intenções de saúde'] = '';
+    this.campos['Aniversários'] = '';
+    this.campos['Ação de graças'] = '';
+  }
+
+  onSubmit() {
+    const valores: { [key: string]: string } = {};
+    this.campoRefs.forEach(ref => {
+      const input = ref.nativeElement as HTMLInputElement;
+      valores[input.name] = input.value;
+    });
+    console.log(valores);
+  }
+
+  addField() {
+    if (Object.keys(this.campos).length >= this.maxCampos) {
+      alert('Você já atingiu o limite máximo de campos.');
+      return;
+    }
+
+    const fieldName = prompt('Digite o nome do novo campo:');
+    if (fieldName && !this.campos[fieldName]) {
+      this.campos[fieldName] = '';
+    } else  {
+      alert('Campo com esse nome já existe.');
+    }
+  }
+
+  getCampoKeys(): string[] {
+    return Object.keys(this.campos);
+  }
 }
